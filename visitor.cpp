953f4d44b76a8d624565a76039932649a72861a1
Siddharth J Singh <siddharthjsingh@protonmail.com>
2021-03-28 01:36:30 +0530

    Added Evaluator

--- visitor.cpp
+++ visitor.cpp
@@ -17,37 +17,36 @@ struct Value : Expression {
     Value(int value) : value(value) {}
 };
 
-struct AdditionExpression : Expression {
+struct Addition : Expression {
     Expression &lhs, &rhs;
-    AdditionExpression(Expression &lhs, Expression &rhs) : lhs(lhs), rhs(rhs) {}
+    Addition(Expression &lhs, Expression &rhs) : lhs(lhs), rhs(rhs) {}
 };
 
-struct MultiplicationExpression : Expression {
+struct Multiplication : Expression {
     Expression &lhs, &rhs;
 
-    MultiplicationExpression(Expression &lhs, Expression &rhs)
+    Multiplication(Expression &lhs, Expression &rhs)
         : lhs(lhs), rhs(rhs) {}
 };
 
-struct ExpressionHandler {
-    virtual void additionHandler(AdditionExpression &expr) = 0;
-    virtual void multiplicationHandler(MultiplicationExpression &expr) = 0;
+struct Handler {
+    virtual void additionHandler(Addition &expr) = 0;
+    virtual void multiplicationHandler(Multiplication &expr) = 0;
     virtual void valueHandler(Value &expr) = 0;
 };
 
-struct ExpressionVisitor : ExpressionHandler {
+struct Visitor : Handler {
     // bind virtual handlers here
-    ExpressionVisitor();
+    Visitor();
+
     std::map<std::type_index, std::function<void(Expression &)>> type_map;
 
     void accept(Expression &expr);
 
-    void castValue(Expression& expr);
-    void castAddition(Expression& expr);
-    void castMultiplication(Expression& expr);
+    template <typename E> void castAndHandle(Expression &expr);
 };
 
-void ExpressionVisitor::accept(Expression &expr) {
+void Visitor::accept(Expression &expr) {
 
     std::type_index exprInfo = std::type_index(typeid(expr));
 
@@ -58,64 +57,70 @@ void ExpressionVisitor::accept(Expression &expr) {
     }
 }
 
-void ExpressionVisitor::castValue(Expression& expr) {
-    auto valExpr = static_cast<Value&>(expr);
+template <> void Visitor::castAndHandle<Value>(Expression &expr) {
+    auto valExpr = static_cast<Value &>(expr);
     valueHandler(valExpr);
 }
 
-void ExpressionVisitor::castAddition(Expression& expr) {
-    auto addExpr = static_cast<AdditionExpression&>(expr);
+template <>
+void Visitor::castAndHandle<Addition>(Expression &expr) {
+    auto addExpr = static_cast<Addition &>(expr);
     additionHandler(addExpr);
 }
 
-void ExpressionVisitor::castMultiplication(Expression& expr) {
-    auto mulExpr = static_cast<MultiplicationExpression&>(expr);
+template <>
+void Visitor::castAndHandle<Multiplication>(
+    Expression &expr) {
+    auto mulExpr = static_cast<Multiplication &>(expr);
     multiplicationHandler(mulExpr);
 }
 
-ExpressionVisitor::ExpressionVisitor() {
+Visitor::Visitor() {
     type_map.insert({std::type_index(typeid(Value)),
-                     std::bind(&ExpressionVisitor::castValue, this,
+                     std::bind(&Visitor::castAndHandle<Value>, this,
                                std::placeholders::_1)});
-    type_map.insert({std::type_index(typeid(AdditionExpression)),
-                     std::bind(&ExpressionVisitor::castAddition, this,
-                               std::placeholders::_1)});
-    type_map.insert({std::type_index(typeid(MultiplicationExpression)),
-                     std::bind(&ExpressionVisitor::castMultiplication, this,
-                               std::placeholders::_1)});
+    type_map.insert(
+        {std::type_index(typeid(Addition)),
+         std::bind(&Visitor::castAndHandle<Addition>, this,
+                   std::placeholders::_1)});
+    type_map.insert(
+        {std::type_index(typeid(Multiplication)),
+         std::bind(&Visitor::castAndHandle<Multiplication>,
+                   this, std::placeholders::_1)});
 };
 
-struct ExpressionPrinter : ExpressionVisitor {
+struct Printer : Visitor {
     ostringstream oss;
 
-    ExpressionPrinter() : ExpressionVisitor{} {};
+    Printer() : Visitor{} {};
 
-    void additionHandler(AdditionExpression &expr) override;
-    void multiplicationHandler(MultiplicationExpression &expr) override;
+    void additionHandler(Addition &expr) override;
+    void multiplicationHandler(Multiplication &expr) override;
     void valueHandler(Value &expr) override;
 
     string str() const { return oss.str(); }
 };
 
-struct ExpressionEvaluator : ExpressionVisitor {
+struct Evaluator : Visitor {
     int output;
 
-    ExpressionEvaluator() : ExpressionVisitor{} {};
+    Evaluator() : Visitor{} {};
 
-    void additionHandler(AdditionExpression &expr) override;
-    void multiplicationHandler(MultiplicationExpression &expr) override;
+    void additionHandler(Addition &expr) override;
+    void multiplicationHandler(Multiplication &expr) override;
     void valueHandler(Value &expr) override;
 
     int value() const { return output; }
 };
 
-void ExpressionPrinter::multiplicationHandler(MultiplicationExpression &mulExpr) {
+void Printer::multiplicationHandler(
+    Multiplication &mulExpr) {
     accept(mulExpr.lhs);
     oss << "*";
     accept(mulExpr.rhs);
 }
 
-void ExpressionPrinter::additionHandler(AdditionExpression &addExpr) {
+void Printer::additionHandler(Addition &addExpr) {
     oss << "(";
     accept(addExpr.lhs);
     oss << "+";
@@ -123,38 +128,49 @@ void ExpressionPrinter::additionHandler(AdditionExpression &addExpr) {
     oss << ")";
 }
 
-void ExpressionPrinter::valueHandler(Value &valExpr) {
-    oss << valExpr.value;
-}
+void Printer::valueHandler(Value &valExpr) { oss << valExpr.value; }
 
-void ExpressionEvaluator::multiplicationHandler(MultiplicationExpression &mulExpr) {
+void Evaluator::multiplicationHandler(
+    Multiplication &mulExpr) {
     accept(mulExpr.lhs);
+    auto temp = output;
     accept(mulExpr.rhs);
+    output *= temp;
 }
 
-void ExpressionEvaluator::additionHandler(AdditionExpression &addExpr) {
+void Evaluator::additionHandler(Addition &addExpr) {
     accept(addExpr.lhs);
+    auto temp = output;
     accept(addExpr.rhs);
+    output += temp;
 }
 
-void ExpressionEvaluator::valueHandler(Value &expr) {
+void Evaluator::valueHandler(Value &expr) {
+    output = expr.value;
 }
 
 int main() {
-    Value v1{1};
-    Value v2{2};
+    Value v1{6};
+    Value v2{3};
+    Value v3{7};
+    Value v4{5};
+    Value v5{2};
 
-    Expression *add = new AdditionExpression{v1, v2};
-    Expression *mul = new MultiplicationExpression{v1, *add};
+    Expression *add1 = new Addition{v1, v2};
+    Expression *add2 = new Addition{v3, v5};
+    Expression *mul1 = new Multiplication{v4, *add1};
+    Expression *mul2 = new Multiplication{*add1, *add2};
+    Expression *add3 = new Addition{*mul1, *mul2};
+    Expression *mul3 = new Multiplication{*add1, *add3};
 
-    ExpressionPrinter ep;
-    ep.accept(*mul);
+    Printer ep;
+    ep.accept(*mul3);
 
-    /*ExpressionEvaluator ev;
-    ev.accept(*mul);*/
+    Evaluator ev;
+    ev.accept(*mul3);
 
     std::cout << ep.str() << std::endl;
-    //std::cout << ev.value() << std::endl;
+    std::cout << ev.value() << std::endl;
 
     return 0;
 }
